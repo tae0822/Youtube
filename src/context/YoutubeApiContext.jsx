e61a1a4c9e02fe83a48d9c13a14acc410ebc58e1
MisoWan <jopd4@naver.com>
2026-03-17 18:15:15 -0400

    delete FakeYoutubeClient

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,10 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
 import YoutubeClient from '../api/youtubeClient';
-import FakeYoutubeClient from '../api/fakeYotubeClient'
 
 export const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {

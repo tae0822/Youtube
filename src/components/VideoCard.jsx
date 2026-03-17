91a5c285c685a14d38f46e59a3bd45dd2c92b5c7
MisoWan <jopd4@naver.com>
2026-03-17 13:36:50 -0400

    initial commit

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
+
+export default function VideoCard({ video, type }) {
+  const [isError, setIsError] = useState(false);
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+  const isList = type === 'list';
+
+  if (isError) return null;
+
+  return (
+    <li
+      className={isList ? 'flex gap-1 m-2' : ''}
+      onClick={() => {
+        navigate(`/videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className={isList ? 'w-60 mr-2' : 'w-full'}
+        src={thumbnails.medium.url}
+        alt={title}
+        onError={() => setIsError(true)}
+      />
+      <div>
+        <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+        <p className='text-sm opacity-80'>{channelTitle}</p>
+        <p className='text-sm opacity-80'>{formatAgo(publishedAt)}</p>
+      </div>
+    </li>
+  );
+}
